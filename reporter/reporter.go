5c447087cc635dc7e689c18df7890f099900f87d
Beau Rudder <beau.j.rudder@gmail.com>
2021-03-27 21:27:27 +0000

    GRPC connection reuse

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -17,12 +17,12 @@ var netInterface = flag.String("interface", "", "Interface to bind to")
 
 // Reporter is the struct to handle GRP Comms
 type Reporter struct {
-	address string
-	home    string
+	home string
+	conn *grpc.ClientConn
 }
 
-func (r *Reporter) dial() (*grpc.ClientConn, error) {
-	conn, err := grpc.Dial(r.address, grpc.WithInsecure())
+func dial(address string) (*grpc.ClientConn, error) {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if *netInterface != "" {
 		localAddrDialier := &net.Dialer{
 			LocalAddr: &net.TCPAddr{
@@ -30,32 +30,27 @@ func (r *Reporter) dial() (*grpc.ClientConn, error) {
 				Port: 0,
 			},
 		}
-		conn, err = grpc.Dial(r.address, grpc.WithInsecure(), grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
+		conn, err = grpc.Dial(address, grpc.WithInsecure(), grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
 			return localAddrDialier.DialContext(ctx, "tcp", addr)
 		}))
+
 	}
 	return conn, err
 }
 
 // NewReporter returns a Reporter for gRPC
 func NewReporter(address string, home string) Reporter {
-	return Reporter{home: home, address: address}
+	conn, err := dial(address)
+	if err != nil {
+		log.Print(err)
+	}
+	return Reporter{home: home, conn: conn}
 }
 
 // Address reports pb.AddressRequest to the GRPC server
 func (r *Reporter) Addresses(items []*pb.AddressRequest) error {
 	gAddr := pb.AddressesRequest{Addresses: items}
-	conn, err := r.dial()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err = conn.Close()
-	}()
-	if err != nil {
-		return err
-	}
-	c := pb.NewHomeDetectorClient(conn)
+	c := pb.NewHomeDetectorClient(r.conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*(time.Duration(*timeout)))
 	defer cancel()
 	response, err := c.Addresses(ctx, &gAddr)
@@ -74,17 +69,7 @@ func (r *Reporter) Addresses(items []*pb.AddressRequest) error {
 // Addresses reports pb.AddressesRequest to the GRPC server
 func (r *Reporter) Address(items []*pb.AddressRequest) error {
 	for _, item := range items {
-		conn, err := r.dial()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			err = conn.Close()
-		}()
-		if err != nil {
-			return err
-		}
-		c := pb.NewHomeDetectorClient(conn)
+		c := pb.NewHomeDetectorClient(r.conn)
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*(time.Duration(*timeout)))
 		defer cancel()
 		item.Home = r.home
@@ -105,17 +90,7 @@ func (r *Reporter) Address(items []*pb.AddressRequest) error {
 // Bles is for handling Bluetooth Mac addresses
 func (r *Reporter) Bles(macs []*string) error {
 	for _, mac := range macs {
-		conn, err := grpc.Dial(r.address, grpc.WithInsecure())
-		if err != nil {
-			log.Fatalf("did not connect: %v", err)
-		}
-		defer func() {
-			err = conn.Close()
-		}()
-		if err != nil {
-			log.Fatalf("did not connect: %v", err)
-		}
-		c := pb.NewHomeDetectorClient(conn)
+		c := pb.NewHomeDetectorClient(r.conn)
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second*(time.Duration(*timeout)))
 		defer cancel()
 		response, err := c.Ack(ctx, &pb.BleRequest{Mac: *mac, Home: r.home})
