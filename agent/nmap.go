aa33fdd44177690e0130ab5d9ee08647bc7cf267
Beau Rudder <beau.j.rudder@gmail.com>
2021-04-22 22:29:35 +0100

    ScanNmap linting

--- agent/nmap.go
+++ agent/nmap.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-// Scan executes the nmap binary and parses the result
+// ScanNmap executes the nmap binary and parses the result
 func (reporter *Reporter) ScanNmap(subnet string, home string, localAddrs map[string]string) ([]*pb.AddressRequest, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
